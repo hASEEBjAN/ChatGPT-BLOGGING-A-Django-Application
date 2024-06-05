62d8b69c16140613e91cba2b12e1b4011a3f6649
hASEEBjAN <engrhaseebjan@gmail.com>
2024-06-05 02:39:49 -0700

    Added tags to post and filtering using tags.

--- blog/models.py
+++ blog/models.py
@@ -5,9 +5,16 @@ from django.utils import timezone
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
+
+class Tag(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return self.name
 class Post(models.Model):
     """Model representing a blog post."""
     title = models.CharField(max_length=100)
+    tags = models.ManyToManyField(Tag, related_name='posts', blank=True)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
